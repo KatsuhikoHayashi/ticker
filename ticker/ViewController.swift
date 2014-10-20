f69b875a711ec17ce180cb3d740a821199e56f2a
hayashidesu <katsu-h@w4.dion.ne.jp>
2014-10-20 12:54:40 +0900

    Initial Commit

---
+++ ticker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ticker
+//
+//  Created by 林克彦 on 2014/10/20.
+//  Copyright (c) 2014年 Hayashidesu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
