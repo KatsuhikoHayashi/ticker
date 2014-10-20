6f85df2c43fbea5bc1907453525b38cd4d6d0cea
hayashidesu <katsu-h@w4.dion.ne.jp>
2014-10-20 13:18:42 +0900

    autoscroll

--- ticker/ViewController.swift
+++ ticker/ViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sc: UIScrollView!
+    @IBOutlet weak var lbl: UILabel!
+    
+    var msg = [String]()
+    var msgId = 0
+    var offsetX:CGFloat = 0
+    var changeFlg:Bool = true
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        msg = ["あああああいいいいいうううううえええええおおおおお",
+            "かかかかかきききききくくくくく" ,
+            "さささささしししししすすすすすせせせせせ",
+            "たたたたたちちちちちつつつつつてててててとととととたたたたたちちちちちつつつつつ",
+            "なななななにににににぬぬぬぬぬねねねねねのののののななななな"]
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.04, target: self, selector: Selector("scrollTicker"), userInfo: nil, repeats: true)
+
+    }
+
+    func scrollTicker(){
+        if(changeFlg == true){
+            var offset = sc.contentOffset
+            offset.x = -self.view.frame.size.width
+            sc.contentOffset = offset
+            
+            lbl.text = msg[msgId]
+            msgId++
+            if(msgId >= msg.count){
+                msgId = 0
+            }
+            
+            self.lbl.sizeToFit()
+            offsetX = self.lbl.frame.size.width
+            
+            changeFlg = false
+        }else{
+            var offset = sc.contentOffset
+            offset.x += 3.0;
+            if (offsetX < offset.x) {
+                changeFlg = true
+            }
+            sc.contentOffset = offset
+        }
     }
 
     override func didReceiveMemoryWarning() {
